2155b7b933cf78dcd6b29ed11ac95e85cba5da3d
semgilo <9737935@qq.com>
2021-01-22 11:42:57 +0800

    add pack android

--- modules/config/qc/config.el
+++ modules/config/qc/config.el
@@ -22,20 +22,24 @@
 
 ;; pack android apk
 (defun qc/pack-android(&optional channel)
-  (interactive "sChannel(default qc)):")
+  (interactive)
+  (let ((choice (completing-read-multiple "Select: " '("qc" "qc_official" "lt"))))
+    (setq channel choice))
   (if (string= channel "")
       (setq channel "qc"))
   (setq path (file-name-directory (buffer-file-name)))
   (setq root-path (substring path 0 (string-match "[\w/\\]+?client" path)))
-  (setq bat-path (format "%s/client/build/android/pack_%s.bat" root-path channel))
-  (message bat-path)
-  (call-process-shell-command bat-path))
+  (setq android-build-root (format "%s/client/build/android" root-path))
+  (call-process-shell-command (format "cd %s&start pack_%s.bat" android-build-root channel)))
+
+(map! :map doom-leader-quit/session-map "p" #'qc/pack-android)
 
 ;; svn update
 (defun qc/update-project()
   (interactive)
   (setq path (file-name-directory (buffer-file-name)))
   (setq root-path (substring path 0 (string-match "[\w/\\]+?client" path)))
-  (call-process-shell-command (format "TortoiseProc.exe /command:update:%s /closeonend:2" root-path))
+  (message root-path)
+  (call-process-shell-command (format "TortoiseProc.exe /command:update /path:\"%s\" /closeonend:2" root-path))
   )
 (map! :leader "C-u" #'qc/update-project)
