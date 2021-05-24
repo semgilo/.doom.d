d43bfb47bdf7339f47a623d22c343f9e05c57945
semgilo <9737935@qq.com>
2021-05-24 11:35:43 +0800

    add some custom api

--- modules/config/qc/config.el
+++ modules/config/qc/config.el
@@ -53,9 +53,29 @@
   (call-process-shell-command (format "TortoiseProc.exe /command:update /path:\"%s\" /closeonend:2" root-path))
   )
 
+;; svn commit
+(defun qc/commit-project()
+  (interactive
+   (list (completing-read "Choose: "
+                          '("src" "src+runtime" "src+runtime+cocos2dx" "src+cocos2dx" "cocos2dx+runtime") nil t)))
+  (setq path (file-name-directory (buffer-file-name)))
+  (setq root-path (substring path 0 (string-match "[\w/\\]+?client" path)))
+
+  (call-process-shell-command (format "TortoiseProc.exe /command:update /path:\"%s\" /closeonend:2" root-path))
+  )
+
+(defun qc/open-server-product-dir()
+  (interactive)
+  (setq path (file-name-directory (buffer-file-name)))
+  (setq root-path (substring path 0 (string-match "[\w/\\]+?client" path)))
+  (show-in-explorer (format "%s/server/server_scripts/product" root-path)))
+
 (map! :leader
       (:prefix-map ("m" . "mine")
-       :desc "qcplay pack android"             "p" #'qc/pack-android
+       :desc "qcplay pack android"             "a" #'qc/pack-android
+       :desc "qcplay build ios src"            "i" #'qc/build-ios-src
        :desc "qcplay update project"           "u" #'qc/update-project
        :desc "qcplay launch client"            "c" #'qc/launch-client
-       :desc "qcplay launch server"            "s" #'qc/launch-server))
+       :desc "qcplay launch server"            "s" #'qc/launch-server
+       (:prefix-map ("o" . "show dir in exploder")
+        :desc "server product"   "p" #'qc/open-server-product-dir)))
